5df6ab04ef5549df137bfa3c40515d24fc05fd15
Test Ton <export.from.ecuador@gmail.com>
2019-02-26 20:11:20 +0700

    added view & template 4 post_text NOTE URLS SHOULD B CONVERTED!

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'), # BAD STYLE!! SHOULD USE CLASS-BASED N GENERIC N SHIT LIKE @ DJ TUTORIAL!!!
+    path('post/<str:title>/', views.post_detail, name='post_detail'),
 ]
